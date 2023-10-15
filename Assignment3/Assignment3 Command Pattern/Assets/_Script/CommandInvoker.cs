0e1bf7728c2ec6fbd383dd19d2a7590fdcae18cc
wy6714 <78013025+wy6714@users.noreply.github.com>
2023-10-15 17:23:37 -0400

    create commandInvoker script

---
+++ Assignment3/Assignment3 Command Pattern/Assets/_Script/CommandInvoker.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandInvoker 
+{
+    private Stack<Command> moveHistory = new Stack<Command>();
+
+    public void ExecuteCommand(Command command)
+    {
+        command.Execute();
+        moveHistory.Push(command);
+    }
+
+    public void UndoLastCommand()
+    {
+        if (moveHistory.Count > 0)
+        {
+            Command lastCommand = moveHistory.Pop();
+            lastCommand.Undo();
+            
+        }
+    }
+}
